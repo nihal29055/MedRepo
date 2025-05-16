4bd744c4616c699b199eccad4bcde355595301d1
Nihal Yadav <yadavnihal544@gmail.com>
2025-05-16 23:10:42 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   backend/server.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/TextSummarizer.tsx

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
-const app = require("./app");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
 const connectDB = require("./config/db");
 
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.use(express.json());
 
 connectDB();
 
